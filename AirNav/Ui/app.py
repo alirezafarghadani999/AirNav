cf12fe169da46491382555df30d149d1607ffcf6
Alireza Farghadani <67876027+alirezafarghadani999@users.noreply.github.com>
2023-06-17 20:11:59 +0330

    Add files via upload

---
+++ AirNav/Ui/app.py
@@ -0,0 +1,123 @@
+import tkinter as tk
+from math import cos, sin, radians
+import threading
+import time 
+
+class Ui:
+
+    def create_menu():
+        f= open("Ui/pos.txt", "w")
+        f.write("")
+        f.close()
+        num_options = 10  # تعداد گزینه‌ها
+        radius = 150  # شعاع منو
+        window_width = 500  # عرض پنجره
+        window_height = 500  # ارتفاع پنجره
+        root = tk.Tk()
+        root.title("Circular Menu")
+        pg_loop = True
+    
+
+
+        # محاسبه مختصات مرکز پنجره بر اساس اندازه صفحه نمایش
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        x = int((screen_width / 2) - (window_width / 2))
+        y = int((screen_height / 2) - (window_height / 2))
+
+        # تنظیم مختصات پنجره بر اساس محاسبات قبلی
+        root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+        # root.wm_attributes("-transparentcolor", "#f0f0f0")
+        canvas = tk.Canvas(root, width=window_width, height=window_height)
+        canvas.pack()
+        root.overrideredirect(1) #Remove border
+        root.wm_attributes("-topmost", 1)
+        
+        def hide_window():
+            root.geometry(f"{window_width}x{window_height}+{screen_width}+{screen_height}")
+        def show_window():
+            root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+        with open("Ui/val.txt" ,"w") as f :
+            f.write("False")
+            
+        def loop():
+            while pg_loop:
+                with open("Ui/val.txt" , "r") as f:
+                    if f.read()== "True":
+                        show_window()
+                    else:
+                        hide_window()
+                    
+                with open("Ui/end.txt" ,"r") as f :
+                    if f.read() == "True":
+                       break
+            exit()
+
+        thread1=threading.Thread(target=loop)
+        thread1.start()
+        def option_clicked(option):
+            global end
+            if option == 10:
+                with open("Ui/end.txt" ,"w") as f :
+                    f.write("True")
+                time.sleep(1)
+                root.destroy()
+                exit()
+            elif option == 9:
+                with open("Ui/val.txt" ,"w") as f :
+                    f.write("False")
+            else:
+                print(f"Option {option} clicked!")
+
+        def save_btn_pos(angle,x,y,option):
+
+            with open("Ui/pos.txt", "a+") as f:
+                if option == 10:
+                    f.write(f"{angle};{x};{y};{option}")
+                else:
+                    f.write(f"{angle};{x};{y};{option}\n")
+                    
+                    
+
+        def stay_on_top():
+            root.lift()
+            root.after(200, stay_on_top)
+        # تابعی برای ایجاد گزینه‌ها
+        def create_option(angle, option, real_angle):
+            x = center_x + radius * cos(angle)
+            y = center_y + radius * sin(angle)
+            save_btn_pos(real_angle,x,y,option)
+            option_button = canvas.create_oval(x-30, y-30, x+30, y+30, fill="white")
+            option_text = canvas.create_text(x, y, text=str(option), font=("Arial", 12), fill="black")
+
+            def highlight(event):
+                canvas.itemconfig(option_button, fill="black")
+                canvas.itemconfig(option_text, fill="white")
+
+            def unhighlight(event):
+                canvas.itemconfig(option_button, fill="white")
+                canvas.itemconfig(option_text, fill="black")
+            
+
+            canvas.tag_bind(option_button, "<Enter>", highlight)
+            canvas.tag_bind(option_button, "<Leave>", unhighlight)
+            canvas.tag_bind(option_text, "<Enter>", highlight)
+            canvas.tag_bind(option_text, "<Leave>", unhighlight)
+            canvas.tag_bind(option_button, "<Button-1>", lambda event, option=option: option_clicked(option))
+
+        # محاسبه مختصات مرکز دایره بر اساس اندازه پنجره
+        center_x = int(window_width / 2)
+        center_y = int(window_height / 2)
+
+        # ایجاد گزینه‌ها در دایره
+        for i in range(num_options):
+            angle = radians((360 / num_options) * i)
+            
+            create_option(angle, i + 1,((360 / num_options)*i))
+
+
+        stay_on_top()
+        root.mainloop()
+
+# Ui.create_menu()
---
+++ AirNav/hands_track.py
@@ -0,0 +1,239 @@
+import cv2
+import mediapipe as mp
+import numpy
+import math
+from Ui.app import Ui
+import threading
+import pyautogui
+import tkinter
+
+with open("Ui/end.txt" ,"w") as f :
+    f.write("False")
+
+minx , miny ,maxx, maxy = 0,0,0,0
+minset , maxset = False ,False
+set_openmenu_point = 0
+
+flip = True
+fix_val = 10
+
+hold_1 , hold_2 = 0,0
+hold_time = 20
+while_hold = hold_time
+
+w_screen = tkinter.Tk().winfo_screenwidth()
+h_screen = tkinter.Tk().winfo_screenheight()
+thread1 =  threading.Thread(target=Ui.create_menu)
+
+
+
+def track():
+
+
+
+    def show_menu():
+        with open("Ui/val.txt" ,"w") as f :
+            f.write("True")
+    
+    def check_menu():
+        with open("Ui/val.txt" ,"r") as f :
+            if f.read() == "True":
+                return True
+            else:
+                return False
+
+
+    global minx , miny ,maxx, maxy , minset , maxset , set_openmenu_point , flip , fix_val , hold_1 ,hold_2 ,while_hold
+    global w_screen, h_screen
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
+    
+    cap = cv2.VideoCapture(0)
+    with mp_hands.Hands(
+        static_image_mode=False,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5
+    ) as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Could not read frame from webcam.")
+                break
+
+            def still_run():
+                    with open("Ui/end.txt" ,"r") as f :
+                            if f.read() == "True":
+                                return True
+                            return False
+
+            if hold_1 != 0 and hold_1 != 10 or hold_2 != 0 and hold_2 != 10:
+                if while_hold <= hold_time:
+                    while_hold -= 1
+                    if while_hold == 0:
+                        hold_1 , hold_2 = 0,0
+                        while_hold = hold_time
+            elif hold_1 != 0 and hold_1 != 10 and hold_2 != 0 and hold_2 != 10:
+                if while_hold <= hold_time:
+                    while_hold -= 1
+                    if while_hold == 0:
+                        hold_1 , hold_2 = 0,0
+                        while_hold = hold_time
+                        
+            if hold_1 == 10 or hold_2 == 10:
+                pass
+            if hold_1 == 10 and hold_2 == 10:
+                show_menu()
+            if flip:
+                image = cv2.flip(image, 1)
+
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image_rgb)
+
+            hand1_click , hand2_click = False , False
+            
+            if results.multi_hand_landmarks:
+
+                yc , xc = int(image.shape[0]/2), int(image.shape[1]/2)
+                cv2.circle(image, (xc, yc), 3, (0, 0, 0), -1)
+                cv2.rectangle(image,(minx,miny,abs(maxx-minx),abs(maxy-miny)),(0,0,255),1)
+                
+                if results.multi_hand_landmarks[0] :
+                        
+                    
+                    finger_landmarks = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.WRIST]
+
+                    xw1, yw1= int(finger_landmarks.x * image.shape[1]), int(finger_landmarks.y * image.shape[0])
+                    cv2.circle(image, (xw1, yw1), 5, (0, 0, 255), -1)
+
+                    finger_landmarks = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.THUMB_TIP]
+                    
+                    xt1, yt1= int(finger_landmarks.x * image.shape[1]), int(finger_landmarks.y * image.shape[0])
+                    cv2.circle(image, (xt1, yt1), 5, (0, 255, 0), -1)
+                    finger_landmarks = results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+
+                    xi1, yi1= int(finger_landmarks.x * image.shape[1]), int(finger_landmarks.y * image.shape[0])
+                    cv2.circle(image, (xi1, yi1), 5, (255, 255, 255), -1)
+
+
+                    if xt1 -fix_val < xi1 < xt1 + fix_val and yt1 -fix_val < yi1 < yt1+fix_val :
+                        hand1_click = True
+                        hold_1 += 1
+                        if check_menu() and while_hold == hold_time and results.multi_handedness[0].classification[0].label == "Right":
+                            pyautogui.click()
+
+                    if results.multi_handedness[0].classification[0].label == "Left":
+                        if minset == False:
+                            minx = xt1
+                            miny = yt1
+
+                    
+                    if results.multi_handedness[0].classification[0].label == "Right":
+                        cv2.line(image,(xw1,yw1),(xc,yc),(0,0,0))
+                        if xw1 >= xc and yw1 <= yc :
+                            angel = numpy.degrees( numpy.arcsin(abs(yc-yw1) / numpy.sqrt(abs(yc-yw1)**2 + abs(xc-xw1)**2)) )
+                        elif xw1 <= xc and yw1 <= yc :
+                            angel = numpy.degrees( numpy.arcsin(abs(xc-xw1) / numpy.sqrt(abs(yc-yw1)**2 + abs(xc-xw1)**2)) ) + 90
+                        elif xw1 <= xc and yw1 >= yc :
+                            angel = numpy.degrees( numpy.arcsin(abs(yc-yw1) / numpy.sqrt(abs(yc-yw1)**2 + abs(xc-xw1)**2)) ) + 180
+                        elif xw1 >= xc and yw1 >= yc :
+                            angel = numpy.degrees( numpy.arcsin(abs(xc-xw1) / numpy.sqrt(abs(yc-yw1)**2 + abs(xc-xw1)**2)) )+ 270
+                        
+                        if check_menu() and not math.isnan(angel):
+                            pos_info= ""
+                            with open("Ui/pos.txt" , "r") as f:
+                                pos_info = f.read()
+                            pos_info = pos_info.split("\n")
+                            for pos in pos_info:
+                                btn_angle = float(pos.split(";")[0])
+                                xb,yb = float(pos.split(";")[1]),float(pos.split(";")[2])
+                                if (btn_angle-10 <= angel <= btn_angle+10):
+                                    pyautogui.moveTo(((w_screen/2)-250+xb), ((h_screen/2)-270+yb))
+
+                        if maxset == False:
+                            maxx = xt1
+                            maxy = yt1
+    
+
+
+                if len(results.multi_hand_landmarks) == 2 :
+                        
+                    finger_landmarks = results.multi_hand_landmarks[1].landmark[mp_hands.HandLandmark.WRIST]
+
+                    xw, yw= int(finger_landmarks.x * image.shape[1]), int(finger_landmarks.y * image.shape[0])
+                    cv2.circle(image, (xw, yw), 5, (0, 0, 255), -1)
+
+                    finger_landmarks = results.multi_hand_landmarks[1].landmark[mp_hands.HandLandmark.THUMB_TIP]
+                    
+                    xt, yt= int(finger_landmarks.x * image.shape[1]), int(finger_landmarks.y * image.shape[0])
+                    cv2.circle(image, (xt, yt), 5, (0, 255, 0), -1)
+                    finger_landmarks = results.multi_hand_landmarks[1].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+
+                    xi, yi= int(finger_landmarks.x * image.shape[1]), int(finger_landmarks.y * image.shape[0])
+                    cv2.circle(image, (xi, yi), 5, (255, 255, 255), -1)
+
+
+
+                    if xt -fix_val < xi < xt + fix_val and yt -fix_val < yi < yt+fix_val :
+                        hand2_click = True
+                        hold_2 += 1
+                        if check_menu() and while_hold == hold_time and results.multi_handedness[0].classification[0].label == "Right":
+                            pyautogui.click()
+
+                    if results.multi_handedness[1].classification[0].label == "Left":
+                        if minset == False:
+                            minx = xt
+                            miny = yt
+
+                    if results.multi_handedness[1].classification[0].label == "Right":
+                        cv2.line(image,(xw,yw),(xc,yc),(0,0,0))
+                        if xw >= xc and yw <= yc :
+                            angel = numpy.degrees( numpy.arcsin(abs(yc-yw) / numpy.sqrt(abs(yc-yw)**2 + abs(xc-xw)**2)) )
+                        elif xw <= xc and yw <= yc :
+                            angel = numpy.degrees( numpy.arcsin(abs(xc-xw) / numpy.sqrt(abs(yc-yw)**2 + abs(xc-xw)**2)) ) + 90
+                        elif xw <= xc and yw >= yc :
+                            angel = numpy.degrees( numpy.arcsin(abs(yc-yw) / numpy.sqrt(abs(yc-yw)**2 + abs(xc-xw)**2)) ) + 180
+                        elif xw >= xc and yw >= yc :
+                            angel = numpy.degrees( numpy.arcsin(abs(xc-xw) / numpy.sqrt(abs(yc-yw)**2 + abs(xc-xw)**2)) )+ 270
+                        
+                        if check_menu() and not math.isnan(angel):
+                            pos_info= ""
+                            with open("Ui/pos.txt" , "r") as f:
+                                pos_info = f.read()
+                            pos_info = pos_info.split("\n")
+                            for pos in pos_info:
+                                btn_angle = float(pos.split(";")[0])
+                                xb,yb = float(pos.split(";")[1]),float(pos.split(";")[2])
+                                if (btn_angle-10 <= angel <= btn_angle+10):
+                                    pyautogui.moveTo(((w_screen/2)-250+xb), ((h_screen/2)-270+yb))
+                                    
+
+                        if maxset == False:
+                            maxx = xt
+                            maxy = yt
+
+                    if hand1_click and hand2_click:
+                        minset ,maxset = True, True
+                        set_openmenu_point = 1
+
+                
+
+
+            cv2.imshow("Hand Tracking", image)
+            global thread1, thread
+            if cv2.waitKey(1) & still_run():
+                break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+    exit()
+
+thread = threading.Thread(target=track)
+import time
+thread1.start()
+time.sleep(5)
+thread.start()
+
+thread1.join()
+thread.join()
